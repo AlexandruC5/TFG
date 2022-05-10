8cac99e4d242f367de3d9dfa42c77a5cdd6aefb6
AlexandruC5 <36231161+AlexandruC5@users.noreply.github.com>
2022-05-10 19:02:43 +0200

    fixed error de compilació

--- RED/network.py
+++ RED/network.py
@@ -1,22 +1,20 @@
-#Init network
 
-import random
+ 
 from random import seed
-
-
-
-#Init de la red on se li fique una hidden layer y un output layer que sera el retorn. 
+from random import random
+ 
+# Init a network
 def init_network(n_inputs, n_hidden, n_outputs):
-    network = list()
-    hidden_layer =[{'weights': [random() for i in range(n_inputs +1)]} for i in range(n_hidden)]
-    network.append(hidden_layer)
-    output_layer = [{'weights':[random() for i in range(n_hidden +1)]} for i in range(n_outputs)]
-    network.append(output_layer)
-    return network
-
-
-
+	network = list()
+	hidden_layer = [{'weights':[random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
+	network.append(hidden_layer)
+	output_layer = [{'weights':[random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
+	network.append(output_layer)
+	return network
+ 
 seed(1)
-network = init_network(2,1,2)
+network = init_network(2, 1, 2)
+for layer in network:
+	print(layer)
 
-for layer in network: print(layer)
+    
