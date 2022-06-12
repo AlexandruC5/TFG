8255f2f32baf1c30845a823c09001cca8a7f3ae3
AlexandruC5 <36231161+AlexandruC5@users.noreply.github.com>
2022-06-12 19:02:23 +0200

    Update table.py
    - fixed base table pos

--- Generator/table.py
+++ Generator/table.py
@@ -1,26 +1,20 @@
 # Esto esta hecho para generar una base de una mesa
-# con formas basicas usando la libreria VPython. 
-from vpython import box, vector, cylinder, color
+# con formas basicas usando la libreria VPython.
+from matplotlib.pyplot import axis
+from vpython import *
+
+# Parte de arriba de la mesa
+top = box(pos=vector(0, 0, 0),
+          axis=vector(0, 0, 0),
+          size=vector(18, 0.29, 10),
+          up=vector(0, 0, 0),
+          color=color.blue)
 
 
-#Parte de arriba de la mesa
-top = box(pos=vector(-1,4,4), 
-        axis=vector(-1.5,0,-1.5),
-        size=vector(18,0.29,10),
-        up=vector(5,25,5),
-        color = color.blue)
+# Patas de la mesa
 
-
-#Patas de la mesa
-leg1 = cylinder(pos=(-3,-3,0),
-                axis=vector(0,7,0), radius=0.45,
-                color = color.green )
-leg2 = cylinder(pos=vector(3.5,-5,3),
-                axis=vector(0,9,0), radius=0.45,
-                color = color.green )
-leg3 = cylinder(pos=vector(10.5,-3.9,2),
-                axis=vector(0,9,0), radius=0.45,
-                color = color.green )
-leg4 = cylinder(pos=vector(-11.5,-6.5,-2.2),
-                axis=vector(0,11,0), radius=0.45,
-                color = color.green )
+pata1 = cylinder(pos=vector(8.5, 0, -4.5), axis=vector(0, -5, 0), radius=0.5)
+pata2 = cylinder(pos=vector(-8.5, 0, 4.5), axis=vector(0, -5, 0), radius=0.5)
+pata3 = cylinder(pos=vector(8.5, 0, 4.5), axis=vector(0, -5, 0), radius=0.5)
+pata4 = cylinder(pos=vector(-8.5, 0, -4.5),axis=vector(0, -5, 0), radius=0.5)
+ 
