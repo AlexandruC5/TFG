3b6c9e2546f4492d26fb351d6464d09a154c4b2f
AlexandruC5 <36231161+AlexandruC5@users.noreply.github.com>
2022-06-20 19:52:23 +0200

    Changed dataset to local dataset

--- RED/DataSetManager.py
+++ RED/DataSetManager.py
@@ -3,7 +3,9 @@ import fiftyone.zoo as foz
 import torch
 import torchvision
 
-dataset = foz.load_zoo_dataset("quickstart")
+dataset = fo.get_default_dataset_dir("tableDataset")
 session = fo.launch_app(dataset)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
