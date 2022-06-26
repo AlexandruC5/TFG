4990fac9850f7a50a01c05cf357aa4a0a6b71fb8
AlexandruC5 <36231161+AlexandruC5@users.noreply.github.com>
2022-06-26 18:41:25 +0200

    Update DataSetManager.py
    - Define training method

--- RED/DataSetManager.py
+++ RED/DataSetManager.py
@@ -68,3 +68,24 @@ net = Net()
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+
+for epoch in range(2):
+
+    running_loss = 0.0
+
+    for i, data in enumerate(dataset, 0):
+
+        inputs, labels = data
+        optimizer.zero_grad()
+
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if i % 2000 == 1999:
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+
+    print('Finished Training')
